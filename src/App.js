014b54a50bacf4ee0a8044c36f9f7210f1e79c45
AlexMolodoy <sivtsov_a@inbox.ru>
2021-04-29 16:38:17 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import HHH from './details';
 
 function App() {
+
+  const [state, setstate] = useState([])
+
+  const fetchData = () => fetch('http://localhost:7070/api/services').then((data) => data.json()).then((data) => setstate(data));
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+console.log(state)
+
   return (
-    <h1>hello world</h1>
+    <>
+    <Switch>
+      <Route path='/test/:id'>
+        <HHH />
+      </Route>
+    </Switch>
+      <ul>
+        {state.map((element) =>  <li key={element.id} ><Link to={'/test/'+element.id}>{element.name}</Link></li>)}
+      </ul>
+    </>
   );
 }
 
